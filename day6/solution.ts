31c167564e67e4fd64e7bedaa7b28351ff7f2770
Arkadiusz Juszczyk <arkadiuszjuszczyk@hotmail.com>
2022-12-07 18:00:54 +0100

    feat: day6

---
+++ day6/solution.ts
@@ -0,0 +1,21 @@
+
+export function partOne(data: string, chunkSize: number): number {
+  const chars = data.split("");
+
+  let counter = 0;
+
+  for (let i = chunkSize - 1; i < chars.length; i++) {
+    const substring = data.slice(i - chunkSize, i);
+    const substringSet = new Set(substring.split(""));
+    if (substringSet.size === chunkSize) {
+      counter = i;
+      break;
+    }
+  }
+
+  return counter;
+}
+
+export function partTwo(data: string): number {
+  return 2;
+}
---
+++ day6/test.ts
@@ -0,0 +1,43 @@
+import { describe, expect, test } from '@jest/globals';
+import { partOne, partTwo } from './solution';
+
+import { readFile } from 'fs/promises';
+
+const challenge = 'Tuning Trouble';
+
+describe(`Day 6`, () => {
+
+  test(`${challenge} - part one - test data`, async () => {
+    const input: string = await readFile(__dirname + '/test-input.txt', 'utf8');
+    const data = input.split('\n');
+
+    const values = [7, 5, 6, 10, 11];
+
+    data.forEach((item, k) => {
+      expect(partOne(item, 4)).toBe(values[k]);
+    });
+  });
+
+  test(`${challenge} - part one`, async () => {
+    const input: string = await readFile(__dirname + '/input.txt', 'utf8');
+
+    expect(partOne(input, 4)).toBe(1480);
+  });
+
+  test(`${challenge} - part two - test data`, async () => {
+    const input: string = await readFile(__dirname + '/test-input.txt', 'utf8');
+    const data = input.split('\n');
+
+    const values = [19, 23, 23, 29, 26];
+
+    data.forEach((item, k) => {
+      expect(partOne(item, 14)).toBe(values[k]);
+    });
+  });
+
+  test(`${challenge} - part two`, async () => {
+    const input: string = await readFile(__dirname + '/input.txt', 'utf8');
+
+    expect(partOne(input, 14)).toBe(2746);
+  });
+});
